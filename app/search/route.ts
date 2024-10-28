241048624a2f9f9ee5c4c5e3cca1d4bb0095d881
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-10-28 08:26:50 +0700

    Improved search error response

--- app/search/route.ts
+++ app/search/route.ts
@@ -4,7 +4,7 @@ import { searchByFullname } from '../db';
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const query = searchParams.get('query');
-  if (!query) return Response.json({ error: 'query should be provided' });
+  if (!query) return Response.json({ message: 'query should be provided' }, { status: 400 });
 
   const limit = Number(searchParams.get('limit') || 25);
   const skip = Number(searchParams.get('skip') || 0);
