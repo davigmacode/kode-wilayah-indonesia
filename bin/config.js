544b519bb056f5eccef4d288269b7aad4190fe8d
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-10-28 11:16:51 +0700

    Moved generator source to separate dir to prevent import conflict

--- bin/config.js
+++ bin/config.js
@@ -3,7 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_FILE = exports.DB_NAME = exports.DB_PATH = exports.VILLAGES_CODE_LENGTH = exports.DISTRICTS_CODE_LENGTH = exports.REGENCIES_CODE_LENGTH = exports.PROVINCES_CODE_LENGTH = exports.CODE_SEGMENT = exports.CODE_SEPARATOR = exports.FIELD_NAME = exports.FIELD_CODE = exports.DATA_FILE = exports.DATA_FILE_NAME = exports.DATA_PATH = exports.STATIC_PATH = void 0;
+exports.DB_FILE = exports.DB_NAME = exports.DB_PATH = exports.CODE_LENGTHS = exports.VILLAGES_CODE_LENGTH = exports.DISTRICTS_CODE_LENGTH = exports.REGENCIES_CODE_LENGTH = exports.PROVINCES_CODE_LENGTH = exports.CODE_SEGMENT = exports.CODE_SEPARATOR = exports.FIELD_NAME = exports.FIELD_CODE = exports.DATA_FILE = exports.DATA_FILE_NAME = exports.DATA_PATH = exports.STATIC_PATH = void 0;
+exports.isValidResource = isValidResource;
 var path_1 = __importDefault(require("path"));
 exports.STATIC_PATH = process.env.STATIC_PATH || '/public';
 exports.DATA_PATH = process.env.SOURCE_PATH || '/data';
@@ -18,6 +19,9 @@ exports.CODE_SEGMENT = {
     districts: [2, 2, 2],
     villages: [2, 2, 2, 4],
 };
+function isValidResource(k) {
+    return k in exports.CODE_SEGMENT;
+}
 function getCodeLength(resource) {
     var segment = exports.CODE_SEGMENT[resource];
     var separator = exports.CODE_SEPARATOR.length * (segment.length > 1 ? segment.length - 1 : 0);
@@ -27,7 +31,12 @@ exports.PROVINCES_CODE_LENGTH = getCodeLength('provinces');
 exports.REGENCIES_CODE_LENGTH = getCodeLength('regencies');
 exports.DISTRICTS_CODE_LENGTH = getCodeLength('districts');
 exports.VILLAGES_CODE_LENGTH = getCodeLength('villages');
-// export const DB_PATH = path.resolve(__dirname, '../', process.env.DB_PATH || './app/db');
+exports.CODE_LENGTHS = {
+    provinces: exports.PROVINCES_CODE_LENGTH,
+    regencies: exports.REGENCIES_CODE_LENGTH,
+    districts: exports.DISTRICTS_CODE_LENGTH,
+    villages: exports.VILLAGES_CODE_LENGTH,
+};
 exports.DB_PATH = process.env.DB_PATH || 'app/db';
 exports.DB_NAME = process.env.DB_NAME || 'regions.db';
 exports.DB_FILE = path_1.default.join(process.cwd(), exports.DB_PATH, exports.DB_NAME);
